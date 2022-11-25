2e652ce2d0cd449cc932cb9e533bd2e135460329
Claus Hammann <mail@chammann.dev>
2022-11-25 13:10:19 +0100

    init fleet overview and lay initial routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FleetOverviewComponent } from './fleet-overview/fleet-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FleetOverviewComponent
   ],
   imports: [
     BrowserModule,
