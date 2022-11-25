f5c9e3c92ad3be71ce656846c4c1eb96b97a0c36
Claus Hammann <mail@chammann.de>
2022-11-26 00:13:39 +0100

    add api requests to fleet-data service

--- src/app/services/fleet-data.service.ts
+++ src/app/services/fleet-data.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Car, DetailedCar } from '../_models/fleet-data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FleetDataService {
+  private API_URL = environment.API_URL;
+  private FLEET_ID = environment.FLEET_ID;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCars() {
+    return this.http.get<Car[]>(`${this.API_URL}/fleets/${this.FLEET_ID}/cars`);
+  }
+
+  addCar(vin: string) {
+    return this.http
+      .put<Car>(`${this.API_URL}/fleets/${this.FLEET_ID}/cars/${vin}/`, null)
+      .subscribe();
+  }
+
+  getCarDetailed(vin: string) {
+    return this.http.get<DetailedCar>(
+      `${this.API_URL}/fleets/${this.FLEET_ID}/cars/${vin}`
+    );
+  }
+
+  removeCar(vin: string) {
+    return this.http.get<null>(
+      `${this.API_URL}/fleets/${this.FLEET_ID}/cars/${vin}`
+    );
+  }
 }
