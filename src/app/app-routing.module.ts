2d7cbfc907f0b444960703421792177a464815f3
Claus Hammann <mail@chammann.de>
2022-11-25 13:10:19 +0100

    init fleet overview and lay initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FleetOverviewComponent } from './fleet-overview/fleet-overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FleetOverviewComponent },
+  { path: 'overview', component: FleetOverviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/fleet-overview/fleet-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fleet-overview',
+  templateUrl: './fleet-overview.component.html',
+  styleUrls: ['./fleet-overview.component.css']
+})
+export class FleetOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
