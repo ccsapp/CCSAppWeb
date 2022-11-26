c7b053957a66e81a75e285d40c700a70f7a60c3b
Claus Hammann <mail@chammann.dev>
2022-11-26 12:58:15 +0100

    add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
         background: "#292D40",
         foreground: "#1A1F29",
         warning: "#DB5D5D",
+        secondary: "#AFB1D2",
       },
+      fontFamily: {
+        heading: ['Roboto Condensed', 'Helvetica', 'sans-serif'],
+        body: ['Roboto' , 'Helvetica', 'sans-serif']
+      }
     },
   },
   plugins: [],
