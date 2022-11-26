5cdaf77d819d4e452897b6572f8c9ed4df9698c8
Claus Hammann <mail@chammann.de>
2022-11-26 11:17:02 +0100

    add colors to tailwind config and change background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B23296",
+        background: "#292D40",
+        foreground: "#1A1F29",
+        warning: "#DB5D5D",
+      },
+    },
   },
   plugins: [],
 }
