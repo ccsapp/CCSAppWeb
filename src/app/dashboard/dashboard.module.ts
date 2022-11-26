1088bf2cc99667f741408ce4f26dea6002a2b5f8
Claus Hammann <mail@chammann.dev>
2022-11-26 11:24:21 +0100

    remove dashboard routing

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FleetOverviewComponent } from './components/fleet-overview/fleet-overview.component';
-import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
   declarations: [FleetOverviewComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule],
 })
 export class DashboardModule {}
