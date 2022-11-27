308ec27cdce792557485662d01a6121047198991
Claus Hammann <mail@chammann.de>
2022-11-27 16:13:49 +0100

    CC 77 fleet overview

--- src/app/dashboard/components/fleet-overview/fleet-overview.component.ts
+++ src/app/dashboard/components/fleet-overview/fleet-overview.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FleetDataService } from 'src/app/services/fleet-data.service';
+import { Car } from 'src/app/_models/fleet-data';
 
 @Component({
   selector: 'app-fleet-overview',
   templateUrl: './fleet-overview.component.html',
-  styleUrls: ['./fleet-overview.component.css']
+  styleUrls: ['./fleet-overview.component.css'],
 })
 export class FleetOverviewComponent implements OnInit {
+  fleetData$!: Observable<Car[]>;
 
-  constructor() { }
+  constructor(private fleetDataService: FleetDataService) {}
 
   ngOnInit(): void {
+    this.fleetData$ = this.fleetDataService.getCars();
   }
-
 }
