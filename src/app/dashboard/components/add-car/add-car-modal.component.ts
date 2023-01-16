a5266f58e6f3490ebb31d51d6f773d7a052127b9
Claus Hammann <mail@chammann.dev>
2023-01-16 11:31:02 +0100

    CC-146 Frontend Validation

--- src/app/dashboard/components/add-car/add-car-modal.component.ts
+++ src/app/dashboard/components/add-car/add-car-modal.component.ts
@@ -1,9 +1,10 @@
 import {
   HttpErrorResponse,
   HttpResponse,
-  HttpStatusCode
+  HttpStatusCode,
 } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { FleetDataService } from 'src/app/services/fleet-data.service';
 import { ModalService } from 'src/app/services/modal.service';
 
@@ -13,8 +14,10 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./add-car-modal.component.css'],
 })
 export class AddCarModalComponent implements OnInit {
+  @ViewChild('vinField') vinField!: ElementRef;
+  vinInput = new FormControl('');
+
   modalId = 'add-car-modal';
-  vinToAdd: string = '';
   errorMessage?: string;
   loading = false;
 
@@ -26,7 +29,7 @@ export class AddCarModalComponent implements OnInit {
   ngOnInit(): void {
     this.modalService.watch(this.modalId).subscribe((isOpen) => {
       if (!isOpen) {
-        this.vinToAdd = '';
+        this.vinInput.reset();
         this.errorMessage = undefined;
       }
     });
@@ -41,32 +44,40 @@ export class AddCarModalComponent implements OnInit {
   }
 
   onSubmit() {
-    if (!this.vinToAdd) {
-      this.errorMessage = 'Please provide a VIN';
+    // prevent submitting a syntactically invalid VIN or a VIN that was rejected by the backend immediately before
+    // (without changing vinField in the meantime)
+    if (this.vinInput.valid && this.vinInput.dirty) {
+      this.addCar(this.vinInput.value!);
       return;
     }
-    this.errorMessage = undefined;
+    this.vinField.nativeElement.focus();
+  }
+
+  addCar(vin: string) {
+    // mark as not dirty
+    this.vinInput.markAsPristine();
+
+    //syntax validation is handled by the template
     this.loading = true;
-    this.fleetDataService.addCar(this.vinToAdd).subscribe({
+    this.fleetDataService.addCar(vin).subscribe({
       next: (res: HttpResponse<null>) => {
         this.loading = false;
         if (res?.status == HttpStatusCode.NoContent) {
           this.modalService.updateErrorMessage(
-            `The car with VIN "${this.vinToAdd}" already belongs to the fleet.`
+            `The car with VIN "${vin}" already belongs to the fleet.`
           );
         }
+        this.errorMessage = undefined;
         this.closeModal();
       },
       error: (err: HttpErrorResponse) => {
         this.loading = false;
-        if (err?.status == HttpStatusCode.BadRequest) {
-          this.errorMessage = 'Invalid VIN, please try again.';
+        if (err?.status == HttpStatusCode.NotFound) {
+          this.errorMessage = 'The car with the provided VIN was not found.';
           return;
         }
-        const message = err?.error?.message;
-        this.errorMessage = message
-          ? message
-          : 'Communication with the server failed, please try again later';
+        console.log(err?.error?.message);
+        this.errorMessage = 'Communication with the server failed.';
       },
     });
   }
