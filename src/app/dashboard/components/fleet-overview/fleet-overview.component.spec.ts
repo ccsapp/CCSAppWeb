82767c41105dfddf1f08774b676ff242e94d6a9f
Maximilian Müller <maxm123@techie.com>
2023-02-03 22:32:37 +0100

    Resolve CC-193 "Unit tests"

--- src/app/dashboard/components/fleet-overview/fleet-overview.component.spec.ts
+++ src/app/dashboard/components/fleet-overview/fleet-overview.component.spec.ts
@@ -1,18 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FleetOverviewComponent } from './fleet-overview.component';
 
 describe('FleetOverviewComponent', () => {
   let component: FleetOverviewComponent;
   let fixture: ComponentFixture<FleetOverviewComponent>;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(async () => {
+    const hcSpy = jasmine.createSpyObj('HttpClient', ['get', 'put', 'delete']);
+
     await TestBed.configureTestingModule({
       declarations: [FleetOverviewComponent],
+      providers: [{ provide: HttpClient, useValue: hcSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FleetOverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
   it('should create', () => {
