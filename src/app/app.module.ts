9961703a01ae9abc5eba0783e67651b0b02963aa
Claus Hammann <mail@chammann.dev>
2023-02-05 23:57:30 +0100

    implemented new title service and redesigned the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, DashboardModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    DashboardModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
