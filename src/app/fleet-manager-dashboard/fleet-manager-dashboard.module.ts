44546a9232fa0f4fd83d5407bd3db32ef2e844f0
Claus Hammann <mail@chammann.dev>
2023-02-07 19:34:28 +0100

    CC-237 add dynamic car data component

--- src/app/fleet-manager-dashboard/fleet-manager-dashboard.module.ts
+++ src/app/fleet-manager-dashboard/fleet-manager-dashboard.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DynamicDataCardComponent } from '../fleet-manager-dashboard/components/dynamic-data-card/dynamic-data-card.component';
 import { SharedModule } from '../shared/shared.module';
 import { AddCarCardComponent } from './components/add-car-card/add-car-card.component';
+import { CarOverviewCardComponent } from './components/car-overview-card/car-overview-card.component';
 import { FleetOverviewComponent } from './components/fleet-overview/fleet-overview.component';
 import { RemoveCarModalComponent } from './components/remove-car/remove-car-modal.component';
-import { CarOverviewCardComponent } from './components/car-overview-card/car-overview-card.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CarOverviewCardComponent } from './components/car-overview-card/car-ove
     RemoveCarModalComponent,
     AddCarCardComponent,
     CarOverviewCardComponent,
+    DynamicDataCardComponent,
   ],
   imports: [CommonModule, FormsModule, SharedModule, ReactiveFormsModule],
 })
